586eeedb03f4f4973c4d58e144db7424eb193796
Angelina <angie1508nz@gmail.com>
2026-09-15 23:43:23 +1200

    Adjusted audio manager script
    So title BGM continues playing in track selection scene

--- WhackANote/Assets/Scripts/AudioManager.cs
+++ WhackANote/Assets/Scripts/AudioManager.cs
@@ -9,6 +9,22 @@ public class AudioManager : MonoBehaviour
     public AudioClip titleBGM;
     public AudioClip circusTrack;
 
+    public static AudioManager instance;
+
+    private void Awake()
+    {
+        // if this instance has not been created before
+        if (instance == null)
+        {
+            //let instance be this object
+            instance = this;
+            DontDestroyOnLoad(gameObject); // and dont destroy it
+        } 
+        else
+        {
+            Destroy(gameObject); // if it's been created before, destroy it
+        }
+    }
     private void Start()
     {
         musicSource.clip = titleBGM;
